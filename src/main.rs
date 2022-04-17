95f2a16c568321cf2d9e4413885e96da5288788c
FilipHarald <filip.harald@gmail.com>
2022-04-17 10:36:24 -0600

    removed arguments

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use std::io::{self, BufRead};
-use std::env;
 
 const COLOR_BASE: u8 = 128;
 const DELIMETER: &str = "/";
@@ -42,27 +41,14 @@ fn get_color(str: &String) -> [u8; 3] {
     }
 }
 
-fn colorize(str: &&str, color: [u8; 3], escape_prompt: bool) -> String {
+fn colorize(str: &&str, color: [u8; 3]) -> String {
     let r = color[0];
     let g = color[1];
     let b = color[2];
-    if escape_prompt {
-        return format!("\\[\u{001B}[0;38;2;{};{};{}m\\]{}\\[\x1b[00m\\]", r, g, b, str);
-    } else {
-        return format!("\u{001B}[0;38;2;{};{};{}m{}\x1b[00m", r, g, b, str);
-    }
+    return format!("\u{001B}[0;38;2;{};{};{}m{}\x1b[00m", r, g, b, str);
 }
 
 fn main() {
-    let mut escape_prompt = false;
-    for argument in env::args() {
-        if argument == "-p" {
-            escape_prompt = true;
-        }
-        if argument == "-i" {
-            escape_prompt = false;
-        }
-    }
     let stdin = io::stdin();
     for line in stdin.lock().lines() {
         let line = line.expect("¿No?");
@@ -71,7 +57,7 @@ fn main() {
         for (index, word) in words.iter().enumerate() {
             let ancestors_and_me = &words[..index+1].join("");
             let color = get_color(ancestors_and_me);
-            colored_word.push(colorize(word, color, escape_prompt));
+            colored_word.push(colorize(word, color));
         }
         println!("{}", colored_word.join(DELIMETER));
     }

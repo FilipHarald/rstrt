dd82db2e3b0860e3048ad2e6fe1c53e853ecd8ad
filip <filip.harald@gmail.com>
2023-06-10 12:39:00 +0200

    feature: vim status line actually working

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,9 @@ const DELIMETER: &str = "/";
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let is_vim_status_line = args.len() > 1 && args[1] == "--vim-status-line";
+    let is_vim_status_line = args.len() > 2 && args[1] == "--vim-status-line";
     if is_vim_status_line {
-        vim_status_line::colorize_string(DELIMETER);
+        vim_status_line::build_status_line(DELIMETER, &args[2], &args[3]);
     } else {
         let is_ps1_escape = args.len() > 1 && args[1] == "--ps1-escape";
         bash_ansi::colorize_strings(is_ps1_escape, DELIMETER);
--- src/vim_status_line.rs
+++ src/vim_status_line.rs
@@ -1,21 +1,23 @@
-use std::io::{self, BufRead, BufWriter, Write};
+use std::io::{self, BufWriter, Write};
 
 use rainbow_street::get_true_color;
 
-struct XtermDefinition {
-//    background: String,
-//    bold: bool,
-//    ctermbg: String,
-//    ctermfg: u8,
+struct VimHighlight {
     foreground: String,
-//    italic: bool,
-//    reverse: bool,
-//    special: String,
-//    strikethrough: bool,
-//    underline: bool,
+    // TODO: add support for these
+    //    background: String,
+    //    bold: bool,
+    //    ctermbg: u8,
+    // NOTE: if vim is not using termguicolors, then ctermfg will be used. Inspiration here: https://github.com/m00qek/baleia.nvim/blob/main/lua/baleia/colors/xterm.lua
+    //    ctermfg: u8,
+    //    italic: bool,
+    //    reverse: bool,
+    //    special: String,
+    //    strikethrough: bool,
+    //    underline: bool,
 }
 
-impl std::fmt::Debug for XtermDefinition {
+impl std::fmt::Debug for VimHighlight {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.debug_struct("")
             .field("\"foreground\"", &self.foreground)
@@ -23,58 +25,64 @@ impl std::fmt::Debug for XtermDefinition {
     }
 }
 
-struct StatusLine {
+struct VimStatusLine {
     filename_string: String,
-    definitions: Vec<XtermDefinition>,
+    highlights: Vec<VimHighlight>,
 }
 
-impl std::fmt::Debug for StatusLine {
+impl std::fmt::Debug for VimStatusLine {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.debug_struct("")
             .field("\"filename_string\"", &self.filename_string)
-            .field("\"definitions\"", &self.definitions)
+            .field("\"definitions\"", &self.highlights)
             .finish()
     }
 }
 
-pub fn colorize_string(delimiter: &str) {
+pub fn build_status_line(delimiter: &str, file_path: &str, cwd: &str) {
     let mut stream = BufWriter::new(io::stdout());
-    let stdin = io::stdin();
-    let line = stdin.lock().lines().next().unwrap().unwrap();
 
-    let mut status_line_obj: StatusLine = StatusLine {
+    let mut status_line_obj: VimStatusLine = VimStatusLine {
         filename_string: String::new(),
-        definitions: Vec::new(),
+        highlights: Vec::new(),
     };
 
-    let words: Vec<&str> = line.split(delimiter).skip(1).collect();
+    let words: Vec<&str> = file_path.split(delimiter).skip(1).collect();
+    let cwd_words: Vec<&str> = cwd.split(delimiter).skip(1).collect();
 
-    let colorize_from_index = words.len().saturating_sub(9);
+    let build_path_from_index = if words.starts_with(&cwd_words) {
+        cwd_words.len() - 1
+    } else {
+        status_line_obj.filename_string.push_str("/");
+        0
+    };
+    let colorize_from_index = if words.len() > 9 {
+        words.len()
+        .saturating_sub(9)
+        .saturating_add(build_path_from_index)
+    } else {
+        build_path_from_index
+    };
+
     for (index, word) in words.iter().enumerate() {
         let ancestors_and_me = &words[..index + 1].join("");
-        if index >= colorize_from_index {
-            let color = get_true_color(ancestors_and_me);
-            let xterm_definition = XtermDefinition {
-//                background: String::from("none"),
-//                bold: false,
-//                ctermbg: String::from("none"),
-//                ctermfg: 120, // TODO: actually calculate this
-                foreground: format!("#{:02x}{:02x}{:02x}", color[0], color[1], color[2]),
-//                italic: false,
-//                reverse: false,
-//                special: String::from("none"),
-//                strikethrough: false,
-//                underline: false,
-            };
-            status_line_obj.definitions.push(xterm_definition);
-            status_line_obj.filename_string.push_str(&format!(
-                "%{}*",
-                (index.saturating_sub(colorize_from_index) + 1)
-            ));
+        if index >= build_path_from_index {
+            if index >= colorize_from_index {
+                let color = get_true_color(ancestors_and_me);
+                let xterm_definition = VimHighlight {
+                    foreground: format!("#{:02x}{:02x}{:02x}", color[0], color[1], color[2]),
+                };
+                status_line_obj.highlights.push(xterm_definition);
+                status_line_obj.filename_string.push_str(&format!(
+                    "%{}*",
+                    (index.saturating_sub(colorize_from_index) + 1)
+                ));
+            }
+            status_line_obj.filename_string.push_str(word);
+            if index != words.len() - 1 {
+                status_line_obj.filename_string.push_str(delimiter);
+            }
         }
-        status_line_obj
-            .filename_string
-            .push_str(&format!("/{}", word));
     }
     writeln!(&mut stream, "{:?}", status_line_obj).unwrap();
 }
